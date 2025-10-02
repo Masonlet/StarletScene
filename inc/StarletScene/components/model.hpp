592f3664b33a834454c3049121f2c96f77b34218
Masonlet <masonletoile@hotmail.com>
2025-10-02 16:17:12 -0400

    MeshHandle, TextureHandle

--- inc/StarletScene/components/model.hpp
+++ inc/StarletScene/components/model.hpp
@@ -2,6 +2,9 @@
 
 #include "StarletScene/component.hpp"
 
+#include "StarletGraphics/resource/meshHandle.hpp"
+#include "StarletGraphics/resource/textureHandle.hpp"
+
 #include "StarletMath/vec2.hpp"
 
 #include <string>
@@ -15,12 +18,14 @@ enum class ColourMode {
 
 struct Model : public NamedComponent {
 	std::string meshPath;
-	bool isVisible{ true }, isLighted{ true }, useTextures{ false };
+	MeshHandle meshHandle;
 
+	bool isVisible{ true }, isLighted{ true }, useTextures{ false };
 	ColourMode mode{ ColourMode::Solid };
 
 	static const unsigned int NUM_TEXTURES = 4;
 	std::string textureNames[NUM_TEXTURES]{ "" };
 	float textureMixRatio[NUM_TEXTURES]{ 1.0f, 0.0f, 0.0f, 0.0f };
 	Vec2<float> textureTiling{ 1.0f, 1.0f };
+	TextureHandle textureHandles[NUM_TEXTURES];
 };
